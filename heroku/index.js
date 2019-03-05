d2a6895b0b5a9845031a01fcb13923b230a48d61
nguyentienviet123456 <tienviet.nguyen@digimed.vn>
2019-03-05 20:08:04 +0700

    update

--- heroku/index.js
+++ heroku/index.js
@@ -39,6 +39,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 app.post('/facebook', function(req, res) {
   console.log('Facebook request body:', req.body);
   if (!req.isXHubValid()) {
+    console.log(req)
     console.log('Warning - request header X-Hub-Signature not present or invalid');
     res.sendStatus(401);
     return;
